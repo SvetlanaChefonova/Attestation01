6ad7da7703f75de0abdc939bd3d03e58dcd29fe5
SvetlanaChefonova <svetlana_chefonova@mail.ru>
2024-01-29 21:23:55 +0300

    Аттестация01 часть1

--- java/src/App.java
+++ java/src/App.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 // then press Enter. You can now see whitespace characters in your code.
 public class App {
     public static void main(String[] args) throws RuntimeException, IOException {
+
         UsersRepositoryFileImpl repository = new UsersRepositoryFileImpl();
         User user = repository.findById("f1a8a3cb-4ac9-4b3b-8a65-c424e129b9d2");
         System.out.println(user);
